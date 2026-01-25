1b7b3680ad9e1af0ea5fd85ba15f962f6e66be92
pavankumar2964 <pavankumar29ctr@gmail.com>
2026-01-25 11:27:21 +0530

    chittoorcafe

--- src/data/menu.js
+++ src/data/menu.js
@@ -6,14 +6,14 @@ const menu = [
             { name: "Dosa", price: 50, veg: true, image: "/images/menu/dosa.png" },
             { name: "Vada", price: 30, veg: true, image: "/images/menu/vada.png" },
             { name: "Pongal", price: 45, veg: true, image: "/images/menu/ven-pongal-1.webp" },
-            { name: "Puri", price: 50, veg: true, image: "/images/menu/puri.png" }
+            { name: "Puri", price: 50, veg: true, image: "/images/menu/puri.jpeg" }
         ]
     },
     {
         category: "Meals",
         items: [
             { name: "Andhra Veg Meals", price: 120, veg: true, image: "/images/menu/meals.png" },
-            { name: "Andhra Non-Veg Meals", price: 180, veg: false, image: "/images/menu/nonvegmeals.png" },
+            { name: "Andhra Non-Veg Meals", price: 180, veg: false, image: "/images/menu/Andhra-Non-Veg-Meals.jpg" },
             { name: "Ragi Sangati + Natu Kodi Pulusu", price: 220, veg: false, image: "/images/menu/ragi.png" },
             { name: "Chicken Biryani", price: 200, veg: false, image: "/images/menu/biryani.png" }
         ]
@@ -22,8 +22,8 @@ const menu = [
         category: "Beverages",
         items: [
             { name: "Filter Coffee", price: 25, veg: true, image: "/images/menu/coffee.png" },
-            { name: "Tea", price: 15, veg: true, image: "/images/menu/tea.png" },
-            { name: "Badam Milk", price: 30, veg: true, image: "/images/menu/badam.png" }
+            { name: "Tea", price: 15, veg: true, image: "/images/menu/tea.jpeg" },
+            { name: "Badam Milk", price: 30, veg: true, image: "/images/menu/badam.jpeg" }
         ]
     }
 ];
